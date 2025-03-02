a5306a09038f553abd2326495eee4906ea98a218
Utkarsh Yadav <utkarshyadav72a@gmail.com>
2025-03-02 11:51:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,5 @@ def upload_file():
     return jsonify({"status": "File uploaded successfully!", "file": file.filename, "emotion": emotion})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)), debug=True)
+
